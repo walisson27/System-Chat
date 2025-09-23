3a291f40f72e14c776f7caa995a5126fb272014d
walisson27 <walisson_souza7@hotmail.com>
2025-09-23 14:45:34 -0300

    fix: ajuste

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'http://localhost:3000'}})
+const io = require('socket.io')(server, {cors: {origin: 'https://system-chat-5.onrender.com/'}})
 
 const PORT = 3001
 
--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -6,7 +6,7 @@ export default function Join({ setChatVisibility, setSocket, setUsername }) {
 
   const handleJoin = () => {
     if (username.trim()) {
-      const socket = io('http://localhost:3001');
+      const socket = io('https://system-chat-5.onrender.com/');
       socket.emit('join', username);
       setSocket(socket);
       setUsername(username); 
