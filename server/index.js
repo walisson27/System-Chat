ee0a05f384c9971501b64f7fbbd24089fab84b98
walisson27 <walisson_souza7@hotmail.com>
2025-09-23 14:32:45 -0300

    fix: backend

--- server/index.js
+++ server/index.js
@@ -1,36 +1,27 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "https://system-chat-5.onrender.com/", 
-    methods: ["GET", "POST"]
-  }
-});
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:3000'}})
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001
 
-app.get("/", (req, res) => {
-  res.send("Servidor Socket.io rodando 🚀");
-});
-
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('Usuário conectado!', socket.id);
 
-  socket.on('disconnect', () => {
-    console.log('Usuário desconectado!', socket.id);
-  });
+  socket.on('disconnect', reason => {
+    console.log('Usuário desconectado!', socket.id)
+  })
 
-  socket.on('set_username', (username) => {
-    socket.data.username = username;
-  });
+  socket.on('set_username', username => {
+    socket.data.username = username
+  })
 
-  socket.on('message', (text) => {
+  socket.on('message', text => {
     io.emit('receive_message', {
       text,
       authorId: socket.id,
-      author: socket.data.username || "Anônimo"
-    });
-  });
-});
+      author: socket.data.username 
+    })
+  })
+})
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+server.listen(PORT, () => console.log('Server running...'))
--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -6,15 +6,10 @@ export default function Join({ setChatVisibility, setSocket, setUsername }) {
 
   const handleJoin = () => {
     if (username.trim()) {
-      const socket = io('https://system-chat-5.onrender.com/', {
-        transports: ['websocket'], 
-      });
-
-      socket.emit('set_username', username);
-
+      const socket = io('http://localhost:3001');
+      socket.emit('join', username);
       setSocket(socket);
-      setUsername(username);
-
+      setUsername(username); 
       setChatVisibility(true);
     }
   };
