7dbf63c44b019921d34bd26538162c833d35a632
walisson27 <walisson_souza7@hotmail.com>
2025-09-27 21:42:05 -0300

    fix: envio join entre mesagens dos users

--- server/index.js
+++ server/index.js
@@ -1,41 +1,42 @@
-const express = require('express')
-const http = require('http')
-const { Server } = require('socket.io')
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://system-chat-new.vercel.app/", 
+    origin: "https://system-chat-new.vercel.app", 
+    methods: ["GET", "POST"]
   }
-})
+});
 
-const PORT = process.env.PORT || 3001
-
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-  res.send("Servidor Socket.io rodando 🚀")
-})
+  res.send("Servidor Socket.io rodando 🚀");
+});
 
 io.on('connection', (socket) => {
-  console.log('Usuário conectado!', socket.id)
+  console.log('Usuário conectado!', socket.id);
 
-  socket.on('disconnect', (reason) => {
-    console.log('Usuário desconectado!', socket.id)
-  })
-
-  socket.on('set_username', (username) => {
-    socket.data.username = username
-  })
+  socket.on('join', (username) => {
+    socket.data.username = username;
+    console.log(`Usuário ${username} entrou com ID: ${socket.id}`);
+  });
 
   socket.on('message', (text) => {
     io.emit('receive_message', {
       text,
       authorId: socket.id,
       author: socket.data.username || "Anônimo"
-    })
-  })
-})
+    });
+  });
 
-server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`))
+  socket.on('disconnect', () => {
+    console.log('Usuário desconectado!', socket.id);
+  });
+});
+
+server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
